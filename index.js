6436a1b9a336a1281fda5ca120c9bf8d5e64d65d
Meem Fatema <meemfatema95@gmail.com>
2024-06-08 02:04:42 -0400

    create add property get and post route

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-const {
-  MongoClient,
-  ServerApiVersion,
-  ObjectId
-} = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
-  origin: [
-     'http://localhost:5174'
-    // 'https://h-food-396f7.web.app',
-    //'https://h-food-396f7.firebaseapp.com'
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      // 'https://h-food-396f7.web.app',
+      //'https://h-food-396f7.firebaseapp.com'
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
-
-
 
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
@@ -40,49 +36,60 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
   if (!token) {
-      return res.status(401).send({ message: 'unauthorized access' })
-     
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-          return res.status(401).send({ message: 'unauthorized access' })
-      }
-      req.user = decoded;
-      next();
-  })
-}
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
 };
 
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db('real-estate').collection('user');
-    
+    const userCollection = client.db("real-estate").collection("user");
+    const propertyCollection = client.db("real-estate").collection("property");
 
     // user related apis
-    app.post('/jwt', async (req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log('user for token', user);
+      console.log("user for token", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1h'
+        expiresIn: "1h",
       });
 
-      res.cookie('token', token, cookieOptions)
-        .send({
-          success: true
-        });
-    })
+      res.cookie("token", token, cookieOptions).send({
+        success: true,
+      });
+    });
+    //all property routes
+    //get property
+
+    app.get("/property", async (req, res) => {
+      const cursor = propertyCollection.find();
+      const property = await cursor.toArray();
+      res.send(property);
+    });
+    app.post("/property", async (req, res) => {
+      const newProperty = req.body;
+      console.log(newProperty);
+      const result = await propertyCollection.insertOne(newProperty);
+      res.send(result);
+    });
+
     ///Logout
     app.post("/logout", async (req, res) => {
       const user = req.body;
@@ -92,40 +99,36 @@ async function run() {
         .send({ success: true });
     });
 
-    app.get('/user', async (req, res) => {
+    app.get("/user", async (req, res) => {
       const cursor = userCollection.find();
       const users = await cursor.toArray();
       res.send(users);
-    })
+    });
 
-    app.post('/user', async (req, res) => {
+    app.post("/user", async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
-    app.patch('/user', async (req, res) => {
+    app.patch("/user", async (req, res) => {
       const user = req.body;
       const filter = {
-        email: user.email
-      }
+        email: user.email,
+      };
       const updateDoc = {
         $set: {
-          lastLoggedAt: user.lastLoggedAt
-        }
-      }
+          lastLoggedAt: user.lastLoggedAt,
+        },
+      };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
-
-
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({
-      ping: 1
+      ping: 1,
     });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
