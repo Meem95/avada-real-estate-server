a2ae84f458f0ee142b50ccfa75c69ef36a5e9f02
Meem Fatema <meemfatema95@gmail.com>
2024-06-20 02:19:57 -0400

    deploy backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -12,9 +12,9 @@ const port = process.env.PORT || 5000;
 // app.use(
 //   cors({
 //     origin: [
-//       "http://localhost:5173",
-//       // 'https://h-food-396f7.web.app',
-//       //'https://h-food-396f7.firebaseapp.com'
+//       //"http://localhost:5173",
+//       'https://b9a12-real-estate.web.app',
+//       'https://b9a12-real-estate.firebaseapp.com'
 //     ],
 //     credentials: true,
 //   })
@@ -37,16 +37,16 @@ const client = new MongoClient(uri, {
   },
 });
 
-const cookieOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-};
+// const cookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+// };
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     const userCollection = client.db("real-estate").collection("user");
     const propertyCollection = client.db("real-estate").collection("property");
@@ -256,6 +256,20 @@ async function run() {
         },
       };
       const result = await propertyCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.patch("/agent-status/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          agentRole: "fraud"
+        },
+      };
+      const result = await propertyCollection.updateOne(filter, updatedDoc);
+      console.log("all console log",result)
       res.send(result);
     });
     app.get("/property/:id", async (req, res) => {
